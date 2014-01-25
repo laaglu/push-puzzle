ab479830b0c18a21546aff84a7332e46d887b1c0
Lukas Laag <laaglu@gmail.com>
2014-01-25 14:51:12 +0100

    Fixed build script. Added missing jquery dependency to git repo

--- build.sh
+++ build.sh
@@ -9,8 +9,10 @@ rm -rf ../push-puzzle.optimized/js/vendor/require-2.1.6.js
 rm -rf ../push-puzzle.optimized/js/vendor/libsvg-ide.js
 rm -rf ../push-puzzle.optimized/.idea
 rm -rf ../push-puzzle.optimized/.git
-sed -e "s@<html class='no-js'>@<html manifest='index.appcache' class='no-js'>@g" index.html | sed -e "s@<script data-main='js/main' src='js/vendor/require-2.1.6.js'></script>@<script type='text/javascript' src='js/main.js'></script>@g" > ../push-puzzle.js.optimized/index.html
+sed -e "s@<html>@<html manifest='index.appcache'>@g" index.html | sed -e "s@<script data-main='js/main' src='js/vendor/require-2.1.6.js'></script>@<script type='text/javascript' src='js/main.js'></script>@g" > ../push-puzzle.optimized/index.html
 rm -rf ../push-puzzle.zip
-sleep 1
-touch ../push-puzzle.optimized/index.appcache
+DATE=`date`
+VERSION=`grep version manifest.webapp | sed -e 's/[^0-9.]*//g'`
+EXPR="s/#.*/#v${VERSION} ${DATE}/g"
+sed -e "${EXPR}" index.appcache > ../push-puzzle.optimized/index.appcache
 ( cd ../push-puzzle.optimized ; zip -9 -v -r ../push-puzzle.zip .)
