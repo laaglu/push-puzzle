893937753f26900dc32e06c3ad46189b9b4c0d59
Lukas Laag <laaglu@gmail.com>
2014-05-09 17:38:21 +0200

    Fixed brunch/bower build to include moment.js language scripts. Added README.md

---
+++ README.md
@@ -0,0 +1,19 @@
+# Overview
+PushPuzzle is a FirefoxOS / OpenWebApp version of the classic puzzle game. 
+
+This github repository contains the full code of the application. You can use also the repository to view/file bug reports and feature requests
+
+Users can download the app and read the user guide at: [fos-games.org](http://www.fos-games.org/)
+They may also want to run the app directly from the [Firefox Marketplace](https://marketplace.firefox.com/app/pushpuzzle/)
+
+# Build
+To build the pushpuzzle webapp, all you need is a recent [node.js](http://nodejs.org/) installation. The app build relies entirely on [brunch](http://brunch.io/) and [bower](http://bower.io/).
+
+Then run the following commands:
+```
+  sudo npm install -g bower
+  git clone https://github.com/laaglu/push-puzzle.git
+  cd push-puzzle
+  npm install
+  npm start
+```
--- app/Logger.js
+++ app/Logger.js
@@ -1,28 +1,29 @@
 'use strict';
 
+/*global localStorage*/
 var SHOW_LOGS = 'showLogs';
 var showLogs = localStorage.getItem(SHOW_LOGS);
 var logger = {
   getShowLogs : function() {
-    return showLogs == 'true';
+    return showLogs === 'true';
   },
   setShowLogs : function(value) {
     localStorage.setItem(SHOW_LOGS, value);
     showLogs = value;
   },
   log: function() {
-    if (showLogs == 'true') {
+    if (showLogs === 'true') {
       console.log.apply(console, arguments);
     }
   },
-  error: function(value) {
-    if (showLogs == 'true') {
+  error: function() {
+    if (showLogs === 'true') {
       console.error.apply(console, arguments);
     }
   }
 };
 // If the setting has not been set explicitely, it is off by default.
-if (showLogs == undefined) {
+if (showLogs === null) {
   logger.setShowLogs('false');
 }
 module.exports = logger;
